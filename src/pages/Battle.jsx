710e9842141c68f24a6f3c0dcb68026548ee4917
Giada <giada.bellan@live.it>
2025-05-13 17:42:22 +0200

    battle and leaderboard structure outlined

--- src/pages/Battle.jsx
+++ src/pages/Battle.jsx
@@ -1,8 +1,11 @@
+import RandomOpponent from "../battle components/RandomOpponent";
+
 const Battle = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4">Battle Page</h1>
       <p className="text-lg">This is where the battle will take place!</p>
+      <RandomOpponent />
     </div>
   );
 };
